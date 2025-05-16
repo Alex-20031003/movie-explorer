33cc755f8f3a32aaa4033aab1cfd70678df16ac4
Alex20031003 <oleksiirudenok@gmail.com>
2025-05-16 13:22:35 +0200

    Add Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/components/Navbar.scss';
+
+const Navbar = () => {
+  return (
+    <header className="navbar">
+      <div className="navbar__logo">🎬 MovieExplorer</div>
+    </header>
+  );
+};
+
+export default Navbar;
